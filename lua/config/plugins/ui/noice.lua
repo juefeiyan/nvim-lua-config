b9af4c1e500b8f5f0fad7565bcebfb1f45b9da5a
Juefei Yan <jyan@indeed.com>
2024-02-29 12:01:35 -0800

    disable progress messages

--- lua/config/plugins/ui/noice.lua
+++ lua/config/plugins/ui/noice.lua
@@ -1,6 +1,7 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
+	dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
 	opts = {
 		lsp = {
 			override = {
@@ -8,18 +9,41 @@ return {
 				["vim.lsp.util.stylize_markdown"] = true,
 				["cmp.entry.get_documentation"] = true,
 			},
+			progress = { enabled = false },
+		},
+		messages = {
+			-- NOTE: If you enable messages, then the cmdline is enabled automatically.
+			-- This is a current Neovim limitation.
+			enabled = true, -- enables the Noice messages UI
+			view = "notify", -- default view for messages
+			view_error = "notify", -- view for errors
+			view_warn = "notify", -- view for warnings
+			view_history = "messages", -- view for :messages
+			view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
 		},
 		routes = {
 			{
 				filter = {
 					event = "msg_show",
-					any = {
-						{ find = "%d+L, %d+B" },
-						{ find = "; after #%d+" },
-						{ find = "; before #%d+" },
-					},
+					kind = "",
+					find = "written",
 				},
-				view = "mini",
+				opts = { skip = true },
+			},
+			{
+				filter = {
+					event = "msg_show",
+					kind = "search_count",
+				},
+				opts = { skip = true },
+			},
+			{
+				filter = {
+					event = "msg_show",
+					kind = "",
+					find = "written",
+				},
+				opts = { skip = true },
 			},
 		},
 		presets = {
