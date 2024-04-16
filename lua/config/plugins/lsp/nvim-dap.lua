55f0933d0b5b78e9790da4b796d5913e09453883
Juefei Yan <jyan@indeed.com>
2024-04-16 14:50:15 -0700

    change flash and dap config

--- lua/config/plugins/lsp/nvim-dap.lua
+++ lua/config/plugins/lsp/nvim-dap.lua
@@ -4,14 +4,15 @@ return {
 		local mode_nv = { "n", "v" }
 		local nmappings = {
 			{ from = "<leader>db", to = "<cmd> DapToggleBreakpoint <CR>", mode = mode_nv },
-			{ from = "<leader>dj", to = "<cmd> DapStepInto <CR>", mode = mode_nv },
-			{ from = "<leader>dl", to = "<cmd> DapStepOver <CR>", mode = mode_nv },
+			{ from = "<leader>dl", to = "<cmd> DapStepInto <CR>", mode = mode_nv },
+			{ from = "<leader>dj", to = "<cmd> DapStepOver <CR>", mode = mode_nv },
 			{ from = "<leader>dk", to = "<cmd> DapStepOut <CR>", mode = mode_nv },
 			{ from = "<leader>dq", to = "<cmd> DapTerminate <CR>", mode = mode_nv },
-			{ from = "<leader>df", to = "<cmd> DapRestartFrame <CR>", mode = mode_nv },
+			{ from = "<leader>dr", to = "<cmd> DapRestartFrame <CR>", mode = mode_nv },
 			{ from = "<leader>dc", to = "<cmd> DapContinue <CR>", mode = mode_nv },
 			{ from = "<leader>du", to = "<cmd>lua require('dapui').toggle() <CR>", mode = mode_nv },
-			{ from = "<leader>dt", to = "<cmd>lua require('jdtls').test_class() <CR>", mode = mode_nv },
+			{ from = "<leader>df", to = "<Cmd>lua require'jdtls'.test_class()<CR>", mode = mode_nv },
+			{ from = "<leader>dt", to = "<Cmd>lua require'jdtls'.test_nearest_method()<CR>", mode = mode_nv },
 		}
 		for _, mapping in ipairs(nmappings) do
 			vim.keymap.set(mapping.mode or "n", mapping.from, mapping.to, { noremap = true })
